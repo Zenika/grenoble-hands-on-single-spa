729bc9e850e58e0b5930882da28cf3c963bf6337
Thomas Huchedé <thomas.huchede@zenika.com>
2022-01-10 21:30:55 +0100

    migrate angular app to single-spa

--- packages/city-angular/src/app/app-routing.module.ts
+++ packages/city-angular/src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { CreateComponent } from './modules/create/create.component';
+import {EmptyRouteComponent} from "./empty-route/empty-route.component";
+import {APP_BASE_HREF} from "@angular/common";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create', component: CreateComponent },
+{ path: '**', component: EmptyRouteComponent },
 ];
 
 @NgModule({
+  declarations: [EmptyRouteComponent],
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
 export class AppRoutingModule {}
